6139c61491807f4102d91c657100985444de45ff
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-12-06 18:19:57 -0600

    added an app controller and pulled in the directive

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
 /* global angular */
 
-angular.module('homescreenDemo', [
-    'ngAnimate',
-    'ngMaterial'
-])
-.config([
-    '$mdThemingProvider',
-    function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-        .primaryPalette('green')
-        .accentPalette('blue',{
-            'default': 'A400',
-            'hue-1': 'A700'
-        })
-        .warnPalette('red');
-    }
-]);
+angular.module('homescreenDemo',
+    [
+        'ngAnimate',
+        'ngMaterial',
+        'homescreen'
+    ]
+)
+.config(
+    [
+        '$mdThemingProvider',
+        function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+            .primaryPalette('green')
+            .accentPalette('blue',{
+                'default': 'A400',
+                'hue-1': 'A700'
+            })
+            .warnPalette('red');
+        }
+    ]
+)
+.controller('DemoAppCtrl',
+    [
+        function() {
+            "use strict";
+
+            var vm = this;
+
+            vm.config = {
+                test: 'test'
+            }
+        }
+    ]
+);
