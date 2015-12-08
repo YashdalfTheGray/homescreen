84ebd91f4129c26ce0d5d166f7e90bc963871b95
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-12-08 00:06:43 -0600

    getting started with the background color

--- src/homescreen/homescreen.js
+++ src/homescreen/homescreen.js
@@ -19,6 +19,24 @@ angular.module('homescreen',
                 link: function(scope, element, attr) {
                     if (window.d3) {
                         scope.d3Found = true;
+
+                        console.log(element);
+
+                        var width = element[0].parentElement.clientWidth - 32;
+
+                        var baseSvg = d3.select(element[0])
+                            .append('svg')
+                            .attr('width', width)
+                            .attr('height', scope.config.height)
+                            .style('padding', '8px');
+
+                        var base = baseSvg
+                            .append('rect')
+                            .attr('width', width)
+                            .attr('height', scope.config.height)
+                            .style('fill', scope.config.background);
+
+                        console.log(base);
                     }
                 }
             }
